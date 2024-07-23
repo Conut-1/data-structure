eca8ba05c8c54aeea22ca6383eaf5dbf87b942c1
Conut-1 <1mim1@naver.com>
2024-07-23 12:48:06 +0900

    연결 리스트 테스트 추가

--- Linked List/LinkedList.test.js
+++ Linked List/LinkedList.test.js
@@ -21,6 +21,15 @@ describe("linked list", () => {
     expect(linkedList.size === 4).toBeTruthy();
   });
 
+  it("getNode index out of linked list", () => {
+    linkedList.append(1);
+    linkedList.append(2);
+    linkedList.append(3);
+    linkedList.append(4);
+
+    expect(() => linkedList._getNode(4)).toThrow(LinkedListError);
+  });
+
   it("insertFirst", () => {
     linkedList.append(1);
     linkedList.insertFirst(2);
@@ -51,6 +60,16 @@ describe("linked list", () => {
     expect(linkedList.size === 5).toBeTruthy();
   });
 
+  it("insert index out of list", () => {
+    linkedList.append(1);
+    linkedList.append(2);
+    linkedList.append(3);
+    linkedList.append(4);
+    linkedList.insert(4, 5);
+
+    expect(() => linkedList.insert(6, 6)).toThrow(LinkedListError);
+  });
+
   it("removeFirst", () => {
     linkedList.append(1);
     linkedList.append(2);
@@ -72,6 +91,10 @@ describe("linked list", () => {
     expect(() => linkedList.at(3)).toThrow(LinkedListError);
 
     expect(linkedList.size === 3).toBeTruthy();
+  });
+
+  it("removeFirst remove empty list", () => {
+    expect(linkedList.removeFirst()).toBeNull();
   });
 
   it("remove", () => {
