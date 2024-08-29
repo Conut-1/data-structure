d3a1d6547e6242f4cf85916116c778e346c497d1
Conut-1 <1mim1@naver.com>
2024-08-29 14:59:16 +0900

    내부 알고리즘 분리
    이진 탐색 트리

--- Tree/BinarySearchTree.js
+++ Tree/BinarySearchTree.js
@@ -15,14 +15,18 @@ class BinarySearchTree {
     this.root = rootNode;
   }
 
-  search(data) {
-    let cur = this.root;
+  static _search(node, data) {
+    let cur = node;
 
     while (cur.key !== null && data !== cur.key) {
       cur = data < cur.key ? cur.left : cur.right;
     }
 
-    return cur.key === null ? null : cur.data;
+    return cur;
+  }
+
+  search(data) {
+    return BinarySearchTree._search(this.root, data).key;
   }
 
   insert(data) {}
